907a3f5595d5e03921e85be5ab7354aa47e23550
Emmanuel <sundayemmanuelabraham@gmail.com>
2019-05-28 15:26:14 +0100

    set and configured template engine

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => res.status(200).json({ success: 'Create a developer profile & portfolio, share posts and get help from other developers' }));
+app.use(express.static('public'));
+app.use('/css', express.static('public'))
 
+//Set View Engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.status(200).render('landingpage'));
 if (!module.parent) { app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); }// eslint-disable-line no-console
 
 export default app;
