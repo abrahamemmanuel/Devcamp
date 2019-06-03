58eefebb13903c07ce2840e512394c21d52650a6
Emmanuel <sundayemmanuelabraham@gmail.com>
2019-06-03 10:13:13 +0100

    directory-restructured: separation of concern implemented

--- src/server/server.js
+++ src/server/server.js
@@ -2,16 +2,21 @@ import express from 'express';
 import path from 'path';
 import ejs from 'ejs';
 
+// Create server
 const app = express();
 
+// Set static folder
 app.use(express.static('public'));
 app.use('/css', express.static('public'))
 
-//Set View Engine
+// Set View Engine
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+// Set server to listen to port
 const PORT = process.env.PORT || 5000;
+
+// Get Landing page
 app.get('/', (req, res) => res.status(200).render('landingpage'));
 if (!module.parent) { app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); }// eslint-disable-line no-console
 
--- src/test/landingpage.test.js
+++ src/test/landingpage.test.js
@@ -3,7 +3,8 @@ import app from '../server';
 
 describe('landingpage', () => {
   it('Welcomes the user to the landingpage', (done) => {
-    request(app).get('/')
+    request(app)
+      .get('/')
       .expect(200)
       .expect(/Create a developer profile & portfolio, share posts and get help from other developers/, done);
   });
