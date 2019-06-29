bd47697460b05fc2097dee3821f4324aadf47980
Abraham Emmnauel <sundayemmanuelabraham@gmail.com>
2019-06-29 13:42:07 +0100

    deploy to heroku

--- build/server/config/keys.js
+++ build/server/config/keys.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var key = {
   LOCALDB_URI: 'mongodb://localhost:27017/devcamp',
+  MONGODB_URI: 'mongodb://devcamp_db:pwd123@ds145315.mlab.com:45315/heroku_91c65g66',
   env: process.env.PORT || 5000
 };
 var _default = key;
