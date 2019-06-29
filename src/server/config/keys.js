a8acdc039d6fa2d9f15b884f1f5263de118f7979
Abraham Emmnauel <sundayemmanuelabraham@gmail.com>
2019-06-29 13:07:49 +0100

    added remote DB

--- src/server/config/keys.js
+++ src/server/config/keys.js
@@ -1,5 +1,6 @@
 const key = {
   LOCALDB_URI: 'mongodb://localhost:27017/devcamp',
+  MONGODB_URI: 'mongodb://devcamp_db:pwd123@ds145315.mlab.com:45315/heroku_91c65g66',
   env: process.env.PORT || 5000,
 };
 
