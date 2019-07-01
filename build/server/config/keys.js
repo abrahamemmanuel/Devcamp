b4b97b73eb68b43ad781ae4da3618ff6f8e3839b
Abraham Emmnauel <sundayemmanuelabraham@gmail.com>
2019-07-01 16:51:38 +0100

    added mongoDB_Remot heroku add ons

--- build/server/config/keys.js
+++ build/server/config/keys.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var key = {
   LOCALDB_URI: 'mongodb://localhost:27017/devcamp',
-  MONGODB_URI: 'mongodb://devcamp_db:pwd123@ds145315.mlab.com:45315/heroku_91c65g66',
+  MONGODB_URI: 'mongodb://devcamp_db:devcamp_db1@ds345587.mlab.com:45587/heroku_txg5rq1l',
   env: process.env.PORT || 5000
 };
 var _default = key;
