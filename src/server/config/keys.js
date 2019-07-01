d9e91b3e2bb749cabe49f2113b02723ad9108da4
Abraham Emmnauel <sundayemmanuelabraham@gmail.com>
2019-07-01 16:47:49 +0100

    modified config file

--- src/server/config/keys.js
+++ src/server/config/keys.js
@@ -1,6 +1,6 @@
 const key = {
   LOCALDB_URI: 'mongodb://localhost:27017/devcamp',
-  MONGODB_URI: 'mongodb://devcamp_db:pwd123@ds145315.mlab.com:45315/heroku_91c65g66',
+  MONGODB_URI: 'mongodb://devcamp_db:devcamp_db1@ds345587.mlab.com:45587/heroku_txg5rq1l',
   env: process.env.PORT || 5000,
 };
 
